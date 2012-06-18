7570c70199444ff997c5c1df20cecd62e1c32186
shadyabhi <abhijeet.1989@gmail.com>
2012-06-18 09:29:52 +0530

    Better code, movied timeout function

--- sanitize.js
+++ sanitize.js
@@ -7,7 +7,8 @@ GoogleLinksSanitizer = {
     init : function () {
         //This is when User opens Google's homepage & searches from there (AJAX and shit)
         window.onhashchange = function() {
-            GoogleLinksSanitizer.sanitize();
+            //Sometimes, all_links didn't contain all the links, so I added a delay
+            setTimeout(GoogleLinksSanitizer.sanitize, 800);
             };
         
         //Check if User uses omnibar in chrome to search on google
@@ -17,25 +18,23 @@ GoogleLinksSanitizer = {
         },
 
     sanitize: function () {
-        //Sometimes, all_links didn't contain all the links, so I added a delay
-        setTimeout(function() {
+
             //If there is something after the baseUrl.
-            if (window.location.href.length - window.location.origin.length > 1){
-                var all_main_links = document.getElementsByClassName("r");
-                for (i=0; i<all_main_links.length; i++){
-                    var current_link = all_main_links[i].childNodes[0];
-                    current_link.removeAttribute("onmousedown");
-                    }
-                var osl_links = document.getElementsByClassName("osl")[0].getElementsByTagName("a");
-                for (i=0; i < osl_links; i++){
-                    osl_links[i].removeAttribute("onmousedown");
-                    }
-                var tl_links = document.getElementsByClassName("tl");
-                for (i=0; i < tl_links.length; i++){
-                    tl_links[i].getElementsByTagName("a")[0].removeAttribute("onmousedown");    
-                    }
+        if (window.location.href.length - window.location.origin.length > 1){
+            var all_main_links = document.getElementsByClassName("r");
+            for (i=0; i<all_main_links.length; i++){
+                var current_link = all_main_links[i].childNodes[0];
+                current_link.removeAttribute("onmousedown");
                 }
-            }, 800);
+            var osl_links = document.getElementsByClassName("osl")[0].getElementsByTagName("a");
+            for (i=0; i < osl_links; i++){
+                osl_links[i].removeAttribute("onmousedown");
+                }
+            var tl_links = document.getElementsByClassName("tl");
+            for (i=0; i < tl_links.length; i++){
+                tl_links[i].getElementsByTagName("a")[0].removeAttribute("onmousedown");    
+                }
+            }
         }
 };
 
