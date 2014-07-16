723a411b4dc3bb583b8774d51273de49d64e3f1e
Viranch Mehta <viranch.mehta@gmail.com>
2014-07-16 12:05:32 +0530

    Inject the code for removing event listeners as <script> tag inside <head>
    This resolves the issue of chrome extension's JS running
    in sandboxed environment, which does not allow changing
    the JS context of web page.

--- sanitize.js
+++ sanitize.js
@@ -21,19 +21,32 @@ GoogleLinksSanitizer = {
 
             //If there is something after the baseUrl.
         if (window.location.href.length - window.location.origin.length > 1){
-            var all_main_links = document.getElementsByClassName("r");
-            for (i=0; i<all_main_links.length; i++){
-                var current_link = all_main_links[i].childNodes[0];
-                current_link.removeAttribute("onmousedown");
-                }
-            var osl_links = document.getElementsByClassName("osl")[0].getElementsByTagName("a");
-            for (i=0; i < osl_links; i++){
-                osl_links[i].removeAttribute("onmousedown");
-                }
-            var tl_links = document.getElementsByClassName("tl");
-            for (i=0; i < tl_links.length; i++){
-                tl_links[i].getElementsByTagName("a")[0].removeAttribute("onmousedown");    
-                }
+            inject = '(' + function() {
+                var all_main_links = document.getElementsByClassName("r");
+                for (i=0; i<all_main_links.length; i++){
+                    var current_link = all_main_links[i].childNodes[0];
+                    current_link.onmousedown = null;
+                    }
+
+                var osl_links = document.getElementsByClassName("osl");
+                if (osl_links.length > 0) {
+                    osl_links = osl_links[0].getElementsByTagName("a");
+                    for (i=0; i < osl_links.length; i++){
+                        osl_links[i].onmousedown = null;
+                        }
+                    }
+
+                var tl_links = document.getElementsByClassName("tl");
+                for (i=0; i < tl_links.length; i++){
+                    tl_link = tl_links[i].getElementsByTagName("a");
+                    if (tl_link.length > 0) {
+                        tl_link[0].onmousedown = null;
+                        }
+                    }
+                } + ')();';
+            var script = document.createElement('script');
+            script.textContent = inject;
+            (document.head||document.documentElement).appendChild(script);
             }
         }
 };
